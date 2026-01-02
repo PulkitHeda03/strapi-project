f7c62f98cf63d68aab698ef12ca7b3ad3c3c24d5
PulkitHeda03 <pulkitheda@rathi.com>
2026-01-02 15:16:19 +0530

    Strapi for Finshala and Blogs

--- src/api/finshala/controllers/finshala.js
+++ src/api/finshala/controllers/finshala.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- *  about controller
+ * finshala controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::about.about');
+module.exports = createCoreController('api::finshala.finshala');
--- src/api/finshala/routes/finshala.js
+++ src/api/finshala/routes/finshala.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * about router.
+ * finshala router
  */
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::about.about');
+module.exports = createCoreRouter('api::finshala.finshala');
